16e25c8142cd86f8dfbfb623ea9a214237cc689a
Sullivan Vitiello <sullivanvitiello@gmail.com>
2018-07-06 02:09:56 +0200

    Aff custom product page and add Swatch snippet

--- src/assets/custom.js
+++ src/assets/custom.js
@@ -11,3 +11,22 @@ $(document).ready(function() {
       resizeImage();
   });
 });
+
+
+jQuery(function() {
+  jQuery('.swatch :radio').change(function() {
+    var optionIndex = jQuery(this).closest('.swatch').attr('data-option-index');
+    var optionValue = jQuery(this).val();
+    jQuery(this)
+      .closest('form')
+      .find('.single-option-selector')
+      .eq(optionIndex)
+      .val(optionValue)
+      .trigger('change');
+  });
+});
+
+$('.swatch-element').on('click', function(){
+  $(this).parent().find('.swatch-element').removeClass('selected');
+  $(this).addClass('selected');
+});
