fbc42cbaecaee4eebbbce3a487daf0d8ceb25aab
Sullivan Vitiello <sullivanvitiello@gmail.com>
2018-07-22 15:43:15 +0200

    Add multiple section to how it works page, init marquee.js for section rolling text

--- dist/assets/custom.js
+++ dist/assets/custom.js
@@ -55,6 +55,41 @@ $(document).ready(function(){
   });
 });
 
+/*********************
+      VIDEO
+*********************/
+
+$(document).ready(function(){
+  $('.video_container').on('click', function() {
+    var video = $(this).find('video').eq(0);
+    if (video.get(0).paused) {
+      $(this).find('.video-button').addClass("hide");
+      video.get(0).play();
+    }
+    else {
+      $(this).find('.video-button').removeClass("hide");
+      video.get(0).pause();
+    }
+  });
+});
+
+/********************
+  Carrousel avis
+********************/
+
+$('.slider-avis-uniq.owl-carousel').owlCarousel({
+  loop:true,
+  items:1,
+  nav:true,
+  dots :false,
+  navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>']
+});
+
+
+/*************************************
+  LIBRARIES
+  TODO : MOVE THE CODE TO ANOTHER FILE
+*************************************/
 
 /*!
  * @preserve
@@ -76,4 +111,108 @@ $('.real_description p span').readmore({
   collapsedHeight : 143
 });
 
-console.log("er");
+/*
+	Vanilla Javascript Marquee
+	Version: 0.1.0
+	Author: Robert Bossaert <https://github.com/robertbossaert>
+	Example call:
+
+	new Marquee('element');
+
+	new Marquee('element', {
+		direction: 'rtl',
+	});
+*/
+var Marquee = function (element, defaults) {
+	"use strict";
+
+	var elem			= document.getElementById(element),
+		options         = (defaults === undefined) ? {} : defaults,
+		continuous		= options.continuous 	|| true,	// once or continuous
+		delayAfter 		= options.delayAfter 	|| 1000,	// pause between loops
+		delayBefore		= options.delayBefore 	|| 0,		// when to start
+		direction 		= options.direction 	|| 'ltr', 	// ltr or rtl
+		loops			= options.loops			|| -1,
+		speed			= options.speed			|| 0.5,
+		timer 			= null,
+		milestone		= 0,
+		marqueeElem		= null,
+		elemWidth		= null,
+		self 			= this,
+		ltrCond			= 0,
+		loopCnt 		= 0,
+		start			= 0,
+		process 		= null,
+		constructor	 	= function (elem) {
+
+			// Build html
+			var elemHTML = elem.innerHTML,
+                elemNode = elem.childNodes[1] || elem;
+
+            elemWidth = elemNode.offsetWidth;
+
+            marqueeElem = '<div>' + elemHTML + '</div>';
+            elem.innerHTML = marqueeElem;
+            marqueeElem = elem.getElementsByTagName('div')[0];
+            elem.style.overflow = 'hidden';
+            marqueeElem.style.whiteSpace = 'nowrap';
+            marqueeElem.style.position = 'relative';
+
+            if (continuous === true) {
+                marqueeElem.innerHTML += elemHTML;
+                marqueeElem.style.width = '200%';
+
+                if (direction === 'ltr') {
+                    start = -elemWidth;
+                }
+            } else {
+                ltrCond = elem.offsetWidth;
+
+                if (direction === 'rtl') {
+                    milestone = ltrCond;
+                }
+            }
+
+            if (direction === 'ltr') {
+                milestone = -elemWidth;
+            } else if (direction === 'rtl') {
+                speed = -speed;
+            }
+
+			self.start();
+
+			return marqueeElem;
+		}
+
+	this.start = function () {
+        process = window.setInterval(function () {
+           self.play();
+        });
+    };
+
+	this.play = function() {
+		// beginning
+		marqueeElem.style.left = start + 'px';
+		start = start + speed;
+
+		if (start > ltrCond || start < -elemWidth) {
+		    start = milestone;
+		    loopCnt++;
+
+		    if (loops !== -1 && loopCnt >= loops) {
+		        marqueeElem.style.left = 0;
+		    }
+		}
+	}
+
+	this.end = function() {
+        window.clearInterval(process);
+	}
+
+	// Init plugin
+	marqueeElem = constructor(elem);
+}
+
+new Marquee('marquee', {
+  direction: 'rtl',
+});
